394764671e8c3f049ecfb1687f0bfab6d7a29c3d
ShobhanKarthick <63546843+ShobhanKarthick@users.noreply.github.com>
2020-07-07 20:22:01 +0530

    modifications for heroku-deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path')
 
 const PORT = process.env.PORT || 4000
 const app = express()
