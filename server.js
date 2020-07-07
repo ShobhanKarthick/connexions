da1d2c66387c855ecc8470a44300d57eec2fab65
ShobhanKarthick <63546843+ShobhanKarthick@users.noreply.github.com>
2020-07-07 19:01:01 +0530

    modifications for heroku-deploy

--- server.js
+++ server.js
@@ -59,6 +59,14 @@ connexionRoutes.route("/single/:id").get((req, res) => {
     })
 })
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+  }
+
 app.use("/connexions", connexionRoutes)
 app.listen(PORT, () => {
     console.log("Database running in PORT:" + PORT)
