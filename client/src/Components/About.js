39af89dfd215d4074ae6faf20a298ae77b056a47
ShobhanKarthick <63546843+ShobhanKarthick@users.noreply.github.com>
2020-07-08 19:08:33 +0530

    about us page completed

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -8,11 +8,65 @@ function About() {
         <Link to="/play">PLAY</Link>
         <Link to="/">HOME</Link>
         </div>
-            <h1 className="about-head">About Us</h1>
-            <div className="about-desc">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+        <fieldset className="about-container">
+        <legend className="about-head">Our Story</legend>
+        <div className="about-desc">
+        We are undergrads of IITM. We just thought why this boredom cannot be killed....and a solution is to that is where you are and what we have built.
+        {
+            // One small tiny teeny creature messed up our lives huh?. Not to worry the 4th dimension will take care. But boredom kills doesn't it.
+            // Don't you wanna kill boredom!
+        }
+        </div>
+        </fieldset>
+        <fieldset id="team" className="about-container">
+        <legend className="about-head">Team</legend>
+        <div className="about-desc">
+            <div className="person">
+            <img className="avatar" src={require('./Images/people-03.svg')} alt="Shobhan"/>
+            <div>
+            <h2 className="name">SHOBHAN</h2>
+            <h5 className="position">Leader</h5>
             </div>
+            </div>
+            <div className="person">
+            <img className="avatar" src={require('./Images/people-04.svg')} alt="Varshini"/>
+            <div>
+            <h2 className="name">VARSHINI</h2>
+            <h5 className="position">Leader</h5>
+            </div>
+            </div>
+            <div className="person">
+            <img className="avatar" src={require('./Images/people-02.svg')} alt="Deva"/>
+            <div>
+            <h2 className="name">DEVA</h2>
+            <h5 className="position">Leader</h5>
+            </div>
+            </div>
+            <div className="person">
+            <img className="avatar" src={require('./Images/people-05.svg')} alt="Sanjay"/>
+            <div>
+            <h2 className="name">SANJAY</h2>
+            <h5 className="position">Leader</h5>
+            </div>
+            </div>
+            <div className="person">
+            <img className="avatar" src={require('./Images/people-03.svg')} alt="Deepak"/>
+            <div>
+            <h2 className="name">DEEPAK</h2>
+            <h5 className="position">Leader</h5>
+            </div>
+            </div>
+            <div className="person">
+            <img className="avatar" src={require('./Images/people-02.svg')} alt="Yoghesh"/>
+            <div>
+            <h2 className="name">YOGHESH</h2>
+            <h5 className="position">Leader</h5>
+            </div>
+            </div>
+            </div>
+        </fieldset>
+        
         </div>
     )
 }
